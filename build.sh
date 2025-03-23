8d35b29f83e8952086dbe445701828add1296c9d
kzwkt <9437935+kzwkt@users.noreply.github.com>
2025-03-24 04:46:01 +0545

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,3 @@
-
-apt update -y
-apt install -y  zip git gcc make ca-certificates binutils libasound2-dev libgtk-layer-shell-dev libgtk-3-dev libjson-c-dev  --no-install-recommends
 git clone --depth=1 https://github.com/LBCrion/sfwbar
 cd sfwbar
 meson setup build -Dbluez=disabled  -Dnm=disabled -Dbsdctl=disabled -Didle=disabled -Didleinhibit=disabled -Dpulse=disabled -Dpipewire=disabled -Dmpd=disabled -Dxkb=disabled -Dbuild-docs=disabled 
