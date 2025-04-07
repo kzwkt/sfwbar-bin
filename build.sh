25d92a3e1f4921ca7e70dfc0e0b72499015f652d
kzwkt <9437935+kzwkt@users.noreply.github.com>
2025-04-07 10:27:03 +0545

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 git clone --depth=1 https://github.com/LBCrion/sfwbar
 cd sfwbar
-meson setup build -Dbluez=disabled  -Dnm=disabled -Dbsdctl=disabled -Didle=disabled -Didleinhibit=disabled -Dpulse=disabled -Dpipewire=disabled -Dmpd=disabled -Dxkb=disabled -Dbuild-docs=disabled 
+meson setup build  -Dappmenu=disabled -Dbluez=disabled -Diwd=disabled  -Dnm=disabled -Dncenter=disabled -Dbsdctl=disabled -Didle=disabled -Didleinhibit=disabled -Dnetwork=disabled -Dpulse=disabled -Dpipewire=disabled -Dmpd=disabled -Dxkb=disabled -Dbuild-docs=disabled  
 ninja -C build
 mkdir sfwb
 cp build/*.so sfwb/
