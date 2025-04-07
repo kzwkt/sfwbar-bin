5b10f3824b5ade3fd2b625f10881e39e35e52ecf
kzwkt <9437935+kzwkt@users.noreply.github.com>
2025-04-07 10:33:39 +0545

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,8 +3,9 @@ cd sfwbar
 meson setup build  -Dappmenu=disabled -Dbluez=disabled -Diwd=disabled  -Dnm=disabled -Dncenter=disabled -Dbsdctl=disabled -Didle=disabled -Didleinhibit=disabled -Dnetwork=disabled -Dpulse=disabled -Dpipewire=disabled -Dmpd=disabled -Dxkb=disabled -Dbuild-docs=disabled  
 ninja -C build
 mkdir sfwb
+strip build/*.so
+strip build/sfwbar
 cp build/*.so sfwb/
 cp build/sfwbar sfwb/
-strip sfwb/*
 cp -r config sfwb/
 zip -r output.zip sfwb/ 
